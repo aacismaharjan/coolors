288586c62f71fc26921dbc9bfb3acde445fca10c
Aashish Maharjan <aashishmaharjan2001@gmail.com>
2020-09-24 23:39:50 +0545

    Updates

---
+++ precache-manifest.eb73e3929c000671cda689e7d1816b0b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1acece769c41ffef96b3bd748cf084de",
+    "url": "/coolors/index.html"
+  },
+  {
+    "revision": "167bf629cf37d2736ed8",
+    "url": "/coolors/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "16709b60403031e5ca2c",
+    "url": "/coolors/static/css/main.a2ce126c.chunk.css"
+  },
+  {
+    "revision": "167bf629cf37d2736ed8",
+    "url": "/coolors/static/js/2.3799329b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/coolors/static/js/2.3799329b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16709b60403031e5ca2c",
+    "url": "/coolors/static/js/main.b3439c24.chunk.js"
+  },
+  {
+    "revision": "c83be0cd993b6ad998e9",
+    "url": "/coolors/static/js/runtime-main.acc02904.js"
+  },
+  {
+    "revision": "eba816ab57fbea29f1a44ddc46ac7fef",
+    "url": "/coolors/static/media/select.eba816ab.mp3"
+  }
+]);
